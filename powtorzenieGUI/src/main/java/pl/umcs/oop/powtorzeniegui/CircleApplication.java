739100fb42e6a108ccce96b19f0eaa54308b786c
Sheodapainter <mgarbiec9@gmail.com>
2025-06-29 22:20:56 +0200

    Update CircleApplication.java
    przekierowanie

--- powtorzenieGUI/src/main/java/pl/umcs/oop/powtorzeniegui/CircleApplication.java
+++ powtorzenieGUI/src/main/java/pl/umcs/oop/powtorzeniegui/CircleApplication.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class CircleApplication extends Application {
+public class CircleApplication extends Application { //tradycyjna funkcja main, patrz plusy
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CircleApplication.class.getResource("app-view.fxml"));
