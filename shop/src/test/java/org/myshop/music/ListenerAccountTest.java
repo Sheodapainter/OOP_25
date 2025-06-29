93742831fb77b7dbbfb166e035b1bd433160c3b0
Sheodapainter <mgarbiec9@gmail.com>
2025-06-29 02:33:44 +0200

    Update ListenerAccountTest.java
    Dodano komentarze

--- shop/src/test/java/org/myshop/music/ListenerAccountTest.java
+++ shop/src/test/java/org/myshop/music/ListenerAccountTest.java
@@ -10,21 +10,21 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ListenerAccountTest {
-    @BeforeAll
+    @BeforeAll //wykonany przed kazdym testem
     public static void connectToDatabase() throws SQLException {
-        DatabaseConnection.connect("songs.db");
-        ListenerAccount.Persistence.init();
+        DatabaseConnection.connect("songs.db"); //laczy sie do bazy danych
+        ListenerAccount.Persistence.init(); //klasa pomocnicza, tworzy tabele jezeli potrzeba
     }
 
     @Test
     public void testRegister() throws SQLException {
         int register_id = ListenerAccount
-                .Persistence.register("user1", "pass123");
-        assertNotEquals(0, register_id);
+                .Persistence.register("user1", "pass123"); //rejestruje uzytkownika, zwraca id
+        assertNotEquals(0, register_id); //test udany jezeli nie jest rowne 0
     }
-    @AfterAll
+    @AfterAll //wykonywany po kazdym tescie
     public static void disconnectFromDatabase() {
-        DatabaseConnection.disconnect();
+        DatabaseConnection.disconnect(); //rozlacza sie z baza danych
     }
 }
 
