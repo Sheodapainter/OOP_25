705b6abc54dbc55cce54759f9e7bcde79fcd7274
Sheodapainter <mgarbiec9@gmail.com>
2025-06-29 21:17:18 +0200

    Update Controller.java
    komentarze

--- powtorzenieGUI/src/main/java/pl/umcs/oop/powtorzeniegui/Controller.java
+++ powtorzenieGUI/src/main/java/pl/umcs/oop/powtorzeniegui/Controller.java
@@ -15,33 +15,36 @@ import java.io.IOException;
 
 public class Controller {
     @FXML
-    private TextField addressField;
+    private TextField addressField; //adres IP
     @FXML
-    private TextField portField;
+    private TextField portField; //port serwera
     @FXML
-    private ColorPicker colorPicker;
+    private ColorPicker colorPicker; //wybor koloru
     @FXML
-    private Canvas canvas;
+    private Canvas canvas; //kanwa
     @FXML
-    private Slider radiusSlider;
+    private Slider radiusSlider; //slider promienia okregu
+    //elementy GUI z pliku view
 
     private ServerThread serverThread;
 
     @FXML
     private void onStartServerClicked() {
         System.out.println("Serwer jest niezależnym programem. Uruchom z klasy Server.");
-    }
+    } //powiadomienie, serwer nie uruchamia sie z aplikacji
 
     @FXML
     private void onConnectClicked() {
         if (serverThread != null && serverThread.isAlive()) {
             System.err.println("Już połączono!");
-            return;
+            return; //nic nie robi jezeli serwer juz dziala
         }
         String address = addressField.getText().trim();
-        int port = Integer.parseInt(portField.getText().trim());
+        int port = Integer.parseInt(portField.getText().trim()); //pobiera dane z pol tekstowych
         try {
+            //tworzy i uruchamia nowy watek z tych pol
             ServerThread newThread = new ServerThread(address, port);
+            //funkcja rysujaca wykonywana po kliknieciu
 
             newThread.setDrawFunction(dot -> {
                 GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -49,7 +52,7 @@ public class Controller {
                 gc.fillOval(dot.x() - dot.radius(), dot.y() - dot.radius(), dot.radius() * 2, dot.radius() * 2);
             });
 
-            newThread.setDaemon(true);
+            newThread.setDaemon(true); //nie blokuje zamkniecia programu
             newThread.start();
             this.serverThread = newThread;
             System.out.println("Połączono z serwerem!");
@@ -60,12 +63,16 @@ public class Controller {
 
     @FXML
     public void onMouseClicked(MouseEvent event) {
+        //pobiera wspolzedne klikniecia
         double x = event.getX();
         double y = event.getY();
+        //pobiera wartosci z odpowiednich pol
         double radius = radiusSlider.getValue();
         Color color = colorPicker.getValue();
 
+        //tworzy nowy dot z danymi wartosciami
         Dot point = new Dot(x, y, radius, color);
+        //wysyla punkt do serwera jesli polaczono
         if (serverThread != null && serverThread.isAlive()) {
             serverThread.send(point);
         } else {
